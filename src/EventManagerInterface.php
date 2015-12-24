149aeaede961b2e37efca21f859e5dde00e88e61
Sebas <dima@t4web.com.ua>
2015-12-24 14:17:24 +0200

    Add EventManagerInterface annotations

--- src/EventManagerInterface.php
+++ src/EventManagerInterface.php
@@ -4,9 +4,24 @@ namespace T4webDomainInterface;
 
 interface EventManagerInterface
 {
+    /**
+     * @param EventInterface $event
+     * @return mixed
+     */
     public function trigger(EventInterface $event);
 
+    /**
+     * @param mixed $event
+     * @param null $listener
+     * @return mixed
+     */
     public function attach($event, $listener = null);
 
+    /**
+     * @param mixed $event
+     * @param EntityInterface|null $entity
+     * @param array $data
+     * @return EventInterface
+     */
     public function createEvent($event, EntityInterface $entity = null, array $data = []);
 }
